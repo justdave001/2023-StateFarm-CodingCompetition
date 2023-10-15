65097e809bcab373bf7b70a294b61d2968927bc2
justdave001 <davidjust20@gmail.com>
2023-10-15 00:08:02 -0500

    changes

---
+++ nodejs/application.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+const SimpleDataTool = require("./controller/simpleDataTool");
+
+console.log("I'm working");
+
+const controller = new SimpleDataTool();
+controller.loadSimpleModels();
